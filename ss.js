6d821537c89d7c3a781280cfb26b907ff6fcce3c
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2025-01-11 20:15:04 +0200

    fg

---
+++ ss.js
@@ -0,0 +1,45 @@
+//1
+
+// const users = [
+//     { id: "player-1", name: "Mango", points: 54, email: 123, balance: 12 },
+//     { id: "player-2", name: "Poly", points: 92, email: 13, balance: 18 },
+//     { id: "player-3", name: "Kiwi", points: 48, email: 143, balance: 16 },
+//     { id: "player-4", name: "Ajax", points: 71, email: 153, balance: 20 },
+//     { id: "player-5", name: "Chelsy", points: 48, email: 183, balance: 30 }
+// ];
+
+// const totalBalance = users.reduce((total, user) => total + user.balance, 0);
+
+// console.log(totalBalance);
+
+
+
+//2
+
+
+// const users = [
+//     { id: "player-1", name: "Mango", points: 54, email: 123, friend: "pol" },
+//     { id: "player-2", name: "Poly", points: 92, email: 13, friend: "markian" },
+//     { id: "player-3", name: "Kiwi", points: 48, email: 143, friend: "tima" },
+//     { id: "player-4", name: "Ajax", points: 71, email: 153, friend: "alina" },
+//     { id: "player-5", name: "Chelsy", points: 48, email: 183, friend: "kirill" }
+// ];
+
+
+// const friendName = "alina"  ;
+
+
+// const usersWithFriend = users.filter(user => user.friend === friendName).map(user => user.name);
+
+// console.log(usersWithFriend);
+
+
+//3
+
+ const users = [
+     { id: "player-1", name: "Mango", points: 54, email: 123, friend: "pol"  },
+     { id: "player-2", name: "Poly", points: 92, email: 13, friend:  },
+     { id: "player-3", name: "Kiwi", points: 48, email: 143, friend: "tima" },
+     { id: "player-4", name: "Ajax", points: 71, email: 153, friend: "alina" },
+     { id: "player-5", name: "Chelsy", points: 48, email: 183, friend:  }
+ ];
