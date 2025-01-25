43f76d4b4a54738c968ca3f5580789543acb526e
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2025-01-25 15:12:23 +0200

    Update ss.js

--- ss.js
+++ ss.js
@@ -36,10 +36,18 @@
 
 //3
 
- const users = [
-     { id: "player-1", name: "Mango", points: 54, email: 123, friend: "pol"  },
-     { id: "player-2", name: "Poly", points: 92, email: 13, friend:  },
-     { id: "player-3", name: "Kiwi", points: 48, email: 143, friend: "tima" },
-     { id: "player-4", name: "Ajax", points: 71, email: 153, friend: "alina" },
-     { id: "player-5", name: "Chelsy", points: 48, email: 183, friend:  }
- ];
+const users = [
+    { id: "player-1", name: "Mango", points: 54, email: 123, friend: 4 },
+    { id: "player-2", name: "Poly", points: 92, email: 13, friend: 0 },
+    { id: "player-3", name: "Kiwi", points: 48, email: 143, friend: 3 },
+    { id: "player-4", name: "Ajax", points: 71, email: 153, friend: 1 },
+    { id: "player-5", name: "Chelsy", points: 48, email: 183, friend: 0 }
+];
+
+
+const sortedUsers = users.sort((a, b) => a.friend - b.friend);
+
+
+const namesSortedByFriends = sortedUsers.map(user => user.name);
+
+console.log(namesSortedByFriends);
